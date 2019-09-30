3360c2b63914cd4b43ab0ae52cff2c43418279f0
Lokesh Devnani <contact@lokeshd.com>
2019-09-30 16:22:24 +0530

    Install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+cd ${ZSH_CUSTOM1:-$ZSH/custom}/plugins; git clone https://github.com/djui/alias-tips.git
