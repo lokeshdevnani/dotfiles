822d8d3fea4980c970fba45f73d0e5d5e65c76f6
Lokesh Devnani <contact@lokeshd.com>
2019-10-10 16:20:31 +0530

    Install uses brew bundle

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+brew bundle
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 cd ${ZSH_CUSTOM1:-$ZSH/custom}/plugins; git clone https://github.com/djui/alias-tips.git
+
